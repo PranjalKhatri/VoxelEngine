163d0ec962addf02d2a31771bae4a7d260364c0b
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-11 16:07:00 +0530

    Added shader program wrapper

--- include/shader.hpp
+++ include/shader.hpp
@@ -3,6 +3,7 @@
 #include "glad/glad.h"
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace pop {
 namespace gfx {
@@ -18,10 +19,12 @@ class Shader {
     ~Shader();
 
     // Loads shader source from file
-    static bool LoadShader(Shader& shader, std::string_view fname);
+    static bool LoadShader(Shader& shader, std::string_view path);
+    // Helper which prints the error as well load and compiles the shader
+    static bool LoadAndCompile(Shader& shader, std::string_view path);
 
     // Compiles the shader
-    bool compile();
+    bool Compile();
 
     // Returns the shader id
     GLuint id() const { return shader_id_; }
@@ -43,5 +46,22 @@ class Shader {
         "Load the Shader before compiling it.";
 };
 
+class ShaderProgram {
+   public:
+    explicit ShaderProgram();
+    ~ShaderProgram();
+    // Attatchs the shader to the program
+    void Attach(const Shader& shader);
+    // Link all the attatched shaders
+    bool Link();
+    // starts using this shader program from now on
+    void use();
+    // Returns last error message (empty if none)
+    std::string GetError() const { return error_msg_; }
+
+   private:
+    GLuint program_id_;
+    std::string error_msg_;
+};
 }  // namespace gfx
 }  // namespace pop
---
+++ src/shader.cpp
@@ -0,0 +1,108 @@
+#include "shader.hpp"
+#include "glad/glad.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+namespace pop {
+namespace gfx {
+
+Shader::Shader(ShaderType stype)
+    : shader_type_{stype},
+      shader_id_{glCreateShader(static_cast<GLuint>(stype))},
+      loaded_{false} {}
+
+Shader::~Shader() {
+    if (shader_id_) {
+        glDeleteShader(shader_id_);
+    }
+}
+
+bool Shader::LoadShader(Shader& shader, std::string_view path) {
+    std::ifstream file{std::string(path)};
+    if (!file) {
+        shader.error_msg_ = "Failed to open shader file.";
+        return false;
+    }
+
+    std::stringstream ss;
+    ss << file.rdbuf();
+    shader.shader_source_ = ss.str();
+    shader.loaded_ = true;
+
+    return true;
+}
+
+bool Shader::LoadAndCompile(Shader& shader, std::string_view path) {
+    if (!LoadShader(shader, path)) {
+        std::cerr << shader.GetError() << "\n";
+        return false;
+    }
+    if (!shader.Compile()) {
+        std::cerr << shader.GetError() << "\n";
+        return false;
+    }
+    return true;
+}
+
+bool Shader::Compile() {
+    if (!loaded_) {
+        error_msg_ = std::string(kNotLoadedMsg);
+        return false;
+    }
+
+    const char* src = shader_source_.c_str();
+    glShaderSource(shader_id_, 1, &src, nullptr);
+    glCompileShader(shader_id_);
+
+    GLint success = 0;
+    glGetShaderiv(shader_id_, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLint logLength = 0;
+        glGetShaderiv(shader_id_, GL_INFO_LOG_LENGTH, &logLength);
+
+        error_msg_.resize(logLength);
+        glGetShaderInfoLog(shader_id_, logLength, nullptr, error_msg_.data());
+        return false;
+    }
+
+    error_msg_.clear();
+    return true;
+}
+
+ShaderProgram::ShaderProgram() : program_id_{glCreateProgram()} {}
+
+ShaderProgram::~ShaderProgram() {
+    if (program_id_) {
+        glDeleteProgram(program_id_);
+    }
+}
+
+void ShaderProgram::Attach(const Shader& shader) {
+    glAttachShader(program_id_, shader.id());
+}
+
+bool ShaderProgram::Link() {
+    glLinkProgram(program_id_);
+
+    GLint success = 0;
+    glGetProgramiv(program_id_, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        GLint logLength;
+        glGetProgramiv(program_id_, GL_INFO_LOG_LENGTH, &logLength);
+
+        error_msg_.resize(logLength);
+        glGetProgramInfoLog(program_id_, logLength, nullptr, error_msg_.data());
+        return false;
+    }
+    error_msg_.clear();
+    return true;
+}
+
+void ShaderProgram::use() {
+    if (program_id_) glUseProgram(program_id_);
+}
+}  // namespace gfx
+}  // namespace pop
