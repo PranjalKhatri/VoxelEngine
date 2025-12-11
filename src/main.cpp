7791104c9061557c5ca59a4ee4d3386d643dec40
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-11 16:09:37 +0530

    Uses new shader wrappers\

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "shader.hpp"
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
-const char* vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char* fragmentShaderSource =
-    "#version 330 core\n"
-    "    out vec4 FragColor;\n"
-    "void main() { FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); }";
 
 int main() {
     // init
@@ -48,42 +38,20 @@ int main() {
         0.9f, -0.5f, 0.0f,   // right
         0.45f, 0.5f, 0.0f    // top
     };
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
-    }
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
-    }
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::LINKING\n" << infoLog << std::endl;
+    pop::gfx::Shader vertexShader{pop::gfx::ShaderType::Vertex};
+    pop::gfx::Shader fragmentShader{pop::gfx::ShaderType::Fragment};
+    if (!pop::gfx::Shader::LoadAndCompile(vertexShader, "shaders/vertex.glsl"))
+        return 1;
+    if (!pop::gfx::Shader::LoadAndCompile(fragmentShader,
+                                          "shaders/fragment.glsl"))
+        return 1;
+    pop::gfx::ShaderProgram shaderProgram{};
+    shaderProgram.Attach(vertexShader);
+    shaderProgram.Attach(fragmentShader);
+    if (!shaderProgram.Link()) {
+        std::cerr << shaderProgram.GetError() << "\n";
+        return 1;
     }
 
     unsigned int VAO;
@@ -92,6 +60,8 @@ int main() {
     // 1. bind Vertex Array Object
     glBindVertexArray(VAO);
     // 2. copy our vertices array in a buffer for OpenGL to use
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     // 3. then set our vertex attributes pointers
@@ -106,7 +76,7 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        shaderProgram.use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         // check and call events and swap the buffers
@@ -114,8 +84,6 @@ int main() {
         glfwPollEvents();
     }
     // cleanup resources and terminate
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
