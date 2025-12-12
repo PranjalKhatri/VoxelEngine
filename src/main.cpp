d90312cff88bec56def9a083c30e22a51863c4d3
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-12 17:16:40 +0530

    update to all the changes in headers and added textures

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "gl_types.hpp"
 #include "shader.hpp"
+#include "vertex_buffers.hpp"
+#include "stb_image.h"
 
 #include <iostream>
+
+using namespace pop::gfx;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -29,24 +34,25 @@ int main() {
     // register callback for resizing
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     float vertices[] = {
-        // first triangle
-        -0.9f, -0.5f, 0.0f,  // left
-        -0.0f, -0.5f, 0.0f,  // right
-        -0.45f, 0.5f, 0.0f,  // top
-                             // second triangle
-        0.0f, -0.5f, 0.0f,   // left
-        0.9f, -0.5f, 0.0f,   // right
-        0.45f, 0.5f, 0.0f    // top
+        // positions          // colors           // texture coords
+        0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+        -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3,  // first triangle
+        1, 2, 3   // second triangle
     };
 
-    pop::gfx::Shader vertexShader{pop::gfx::ShaderType::Vertex};
-    pop::gfx::Shader fragmentShader{pop::gfx::ShaderType::Fragment};
+    Shader vertexShader{ShaderType::Vertex};
+    Shader fragmentShader{ShaderType::Fragment};
     if (!pop::gfx::Shader::LoadAndCompile(vertexShader, "shaders/vertex.glsl"))
         return 1;
     if (!pop::gfx::Shader::LoadAndCompile(fragmentShader,
                                           "shaders/fragment.glsl"))
         return 1;
-    pop::gfx::ShaderProgram shaderProgram{};
+    ShaderProgram shaderProgram{};
     shaderProgram.Attach(vertexShader);
     shaderProgram.Attach(fragmentShader);
     if (!shaderProgram.Link()) {
@@ -54,20 +60,38 @@ int main() {
         return 1;
     }
 
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAO);
+    VertexArray VAO{};
+    VAO.Bind();
     // 2. copy our vertices array in a buffer for OpenGL to use
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLBuffer VBO{BufferType::kArrayBuffer},
+        EBO{BufferType::kElementArrayBuffer};
+    VBO.BufferData(sizeof(vertices), vertices, GL_STATIC_DRAW);
     // 3. then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void*)0);
-    glEnableVertexAttribArray(0);
+    VAO.AddAttribute({0, 3, GLType::kFloat, false, 8 * sizeof(float), 0});
+    VAO.AddAttribute(
+        {1, 3, GLType::kFloat, false, 8 * sizeof(float), 3 * sizeof(float)});
+
+    EBO.Bind();
+    EBO.BufferData(sizeof(indices), indices, GL_STATIC_DRAW);
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int width, height, nchannels;
+    // load rgb data
+    unsigned char* data =
+        stbi_load("textures/wood.jpg", &width, &height, &nchannels, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     // rendering loop
     while (!glfwWindowShouldClose(window)) {
         // input
@@ -76,16 +100,15 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindTexture(GL_TEXTURE_2D, texture);
         shaderProgram.use();
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        VAO.Bind();
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
     // cleanup resources and terminate
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
     glfwTerminate();
     return 0;
 }
