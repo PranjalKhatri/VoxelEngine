68d587fe0f78ceaf241c792f69181e111e547b0b
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-19 21:19:21 +0530

    Added CmdQueue and use unordered set for renderables

--- include/engine.hpp
+++ include/engine.hpp
@@ -2,14 +2,23 @@
 
 #include "camera.hpp"
 #include "renderable.hpp"
+// #include "resource_manager.hpp"
+
 #include <memory>
-#include <unordered_map>
-#include <vector>
+#include <unordered_set>
 
 #include "GLFW/glfw3.h"
 #include "shader.hpp"
 
+#include "safe_queue.hpp"
+
 namespace pop {
+enum class RenderCmdType { Add, Remove };
+
+struct RenderCmd {
+    RenderCmdType               type;
+    std::shared_ptr<Renderable> renderable;
+};
 class Engine {
    public:
     Engine(glm::mat4 projectionMatrix, GLFWwindow* window);
@@ -21,7 +30,15 @@ class Engine {
     // todo define move ctor
 
     void SetMainCamera(std::shared_ptr<gfx::Camera> camera);
-    void AddRenderable(std::shared_ptr<Renderable> renderable);
+
+    void AddRenderable(const RenderCmd& cmd);
+    void RemoveRenderable(const RenderCmd& cmd);
+
+    // void AddRenderable(const std::shared_ptr<Renderable>& renderable);
+    // void RemoveRenderable(const std::shared_ptr<Renderable>& renderable);
+
+    // gfx::ResourceManager&       Resources();
+    // const gfx::ResourceManager& Resources() const;
     void AddShaderProgram(std::unique_ptr<gfx::ShaderProgram> prog);
 
     void Run();
@@ -37,18 +54,23 @@ class Engine {
     void Render();
     void UpdateDeltaTime();
     void ProcessInput();
+    void ProcessCommands();
 
    private:
     float last_frame_time_;
     float delta_time_;
     bool  is_running_;
 
+    CmdQueue<RenderCmd> cmd_queue_;
+
     GLFWwindow* window_;
 
-    std::vector<std::shared_ptr<Renderable>> renderables_;
+    std::unordered_set<std::shared_ptr<Renderable>> renderables_;
     std::unordered_map<gfx::ShaderHandle, std::unique_ptr<gfx::ShaderProgram>>
                                  shader_prog_map_;
     std::shared_ptr<gfx::Camera> main_camera_;
     glm::mat4                    projection_matrix_;
+
+    // gfx::ResourceManager resource_manager_;
 };
 };  // namespace pop
