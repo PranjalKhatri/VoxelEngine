ff1b5ea0efce829dff07ee00b07fd5e9fe90e451
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-23 17:20:49 +0530

    sample nearest for voxel instead of linear

--- src/texture.cpp
+++ src/texture.cpp
@@ -31,9 +31,8 @@ Texture::Texture(TextureType type)
 
     // Default filtering
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                    GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 Texture::~Texture() {
     if (data_) {
