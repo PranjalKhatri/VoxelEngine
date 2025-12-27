8bfe9966bdfd4b935c718d1423c086a31db0ccf6
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-27 22:30:57 +0530

    added stone block

--- src/block_loader.cpp
+++ src/block_loader.cpp
@@ -7,21 +7,25 @@ namespace pop::block {
 void RegisterAllBlocks() {
     auto& reg = BlockRegistry::Get();
 
-    IDs::AIR   = reg.Register(std::make_unique<Block>(Block::Settings{
-          .isSolid = false, .isTranslucent = true, .name = "Air"}));
+    IDs::AIR = reg.Register(std::make_unique<Block>(Block::Settings{
+        .isSolid = false, .isTranslucent = true, .name = "Air"}));
+
     IDs::GRASS = reg.Register(
         std::make_unique<Block>(Block::Settings{.isSolid       = true,
                                                 .isTranslucent = false,
                                                 .uv            = {2, 3},
                                                 .name          = "Grass"}));
-    IDs::DIRT  = reg.Register(std::make_unique<Block>(Block::Settings{
-         .isSolid = true, .isTranslucent = false, .uv = {4}, .name = "Dirt"}));
+
+    IDs::DIRT = reg.Register(std::make_unique<Block>(Block::Settings{
+        .isSolid = true, .isTranslucent = false, .uv = {4}, .name = "Dirt"}));
+
     IDs::WATER = reg.Register(std::make_unique<Block>(Block::Settings{
         .isSolid = false, .isTranslucent = true, .name = "Water"}));
-    IDs::SAND  = reg.Register(std::make_unique<Block>(Block::Settings{
-         .isSolid = true, .isTranslucent = false, .uv = {1}, .name = "Sand"}));
+
+    IDs::SAND = reg.Register(std::make_unique<Block>(Block::Settings{
+        .isSolid = true, .isTranslucent = false, .uv = {1}, .name = "Sand"}));
+
     IDs::STONE = reg.Register(std::make_unique<Block>(Block::Settings{
         .isSolid = true, .isTranslucent = false, .uv = {0}, .name = "Stone"}));
-    // Add more as needed...
 }
 }  // namespace pop::block
