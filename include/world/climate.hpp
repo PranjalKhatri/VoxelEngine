02209757c05898026a5de108bc249feadddc5952
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-12-29 21:02:48 +0530

    temperature and precipitation noise

---
+++ include/world/climate.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "fast_noise_lite.h"
+
+namespace pop::world::climate {
+class ClimateModule {
+   public:
+    struct ClimateSettings {
+        struct TemperatureSettings {
+            float noiseScale = 0.01;
+            float noiseOffset;
+            // in Celcius per block
+            float tempDropRateWithAltitute = 0.1f;
+            int   tempBaseAltitude;
+            float roomTemperature;  // in Celcius
+            float minTemperature;   // in Celcius
+            float maxTemperature;   // in Celcius
+        } temperatureSettings;
+        struct PrecipitationSettings {
+            float noiseScale = 0.01;
+            float noiseOffset;
+            float maxPrecipitation;   // in cm
+            float meanPrecipitation;  // in cm
+            float minPrecipitation;   // in cm
+        } precipitationSettings;
+        int seed = 1337;
+    };
+    ClimateModule(ClimateSettings climateSettings);
+    // return temperature in the range [minTemp,maxTemp] passed in settings
+    float GetTemperatureAt(int x, int y, int z) const;
+    // return precipitation in range [0.0,1.0]
+    float GetPrecipitationAt(int x, int y, int z) const;
+
+   private:
+    ClimateSettings climate_settings_;
+    FastNoiseLite   temperature_noise_;
+    FastNoiseLite   precipitation_noise_;
+};
+};  // namespace pop::world::climate
---
+++ src/climate.cpp
@@ -0,0 +1,48 @@
+#include "world/climate.hpp"
+#include <algorithm>
+#include "fast_noise_lite.h"
+
+namespace pop::world::climate {
+ClimateModule::ClimateModule(ClimateModule::ClimateSettings climateSettnigs)
+    : climate_settings_{climateSettnigs} {
+    const auto &temperatureSettings   = climate_settings_.temperatureSettings;
+    const auto &precipitationSettings = climate_settings_.precipitationSettings;
+
+    auto temperatureSeed =
+        climate_settings_.seed + temperatureSettings.noiseOffset;
+    auto precipitationSeed =
+        climate_settings_.seed + precipitationSettings.noiseOffset;
+
+    temperature_noise_.SetSeed(temperatureSeed);
+    temperature_noise_.SetFrequency(temperatureSettings.noiseScale);
+    temperature_noise_.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+
+    precipitation_noise_.SetSeed(precipitationSeed);
+    precipitation_noise_.SetFrequency(precipitationSettings.noiseScale);
+    temperature_noise_.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+}
+
+float ClimateModule::GetTemperatureAt(int x, int y, int z) const {
+    const auto &s           = climate_settings_.temperatureSettings;
+    float       noiseSample = temperature_noise_.GetNoise((float)x, (float)z);
+
+    float baseTemp =
+        s.roomTemperature + noiseSample * (s.maxTemperature - s.minTemperature);
+
+    int   altitudeDiff = y - s.tempBaseAltitude;
+    float finalTemp    = baseTemp - altitudeDiff * s.tempDropRateWithAltitute;
+    return std::clamp(finalTemp, s.minTemperature, s.maxTemperature);
+}
+
+float ClimateModule::GetPrecipitationAt(int x, int /*y*/, int z) const {
+    const auto &s = climate_settings_.precipitationSettings;
+
+    float noiseSample =
+        (precipitation_noise_.GetNoise((float)x, (float)z) + 1.0) * 0.5;
+
+    float baseHumidity =
+        s.meanPrecipitation +
+        noiseSample * (s.maxPrecipitation - s.minPrecipitation);
+    return std::clamp(baseHumidity, 0.0f, 1.0f);
+}
+};  // namespace pop::world::climate
